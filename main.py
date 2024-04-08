05361eb349c83293e58fa5f49b4bb0eb0f7b7513
Yash Pradhan <iamyashpradhan@gmail.com>
2024-04-08 19:04:54 -0400

    Changes

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.write('CMPD Traffic Stops')
+
+
+
+@st.cache_data  # 👈 Add the caching decorator
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+
+stops = load_data('data/Officer_Traffic_Stops.csv')
+st.dataframe(stops)
